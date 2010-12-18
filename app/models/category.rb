4bc883b5cb81e3aed7d7f9da35ad4808b6fb8ab8
Pawel Kuzniar <pawel@pawel-kuzniars-macbook-pro.local>
2010-12-18 23:11:47 +0100

    Added unique permalink to categories.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,8 @@
 class Category < ActiveRecord::Base
-      has_many :category_items, :dependent => :destroy
-      acts_as_tree
-
-      def to_param
-      	  permalink      
-      end
-
-#      def children
-#      	  Category.find(:all, :conditions => {:parent_id => self.id})
-#      end  
-
+  has_many :category_items, :dependent => :destroy
+  acts_as_tree
+  
+  def to_param
+    permalink      
+  end
 end
--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,4 +1,4 @@
 class ProductCategory < Category
       has_many :products, :through => :category_items, :source => :item, :source_type => "Product"
-      has_permalink :name, :to_param => :permalink
+      has_permalink :name, :unique => true, :to_param => :permalink
 end
