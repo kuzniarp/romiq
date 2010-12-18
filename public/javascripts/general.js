5a355b29da6aee83b5bfa1259b562765c18b5e01
kuzniarp <kuzniarp@gmail.com>
2010-12-18 23:54:19 +0100

    UPdated hp styles.

--- public/javascripts/general.js
+++ public/javascripts/general.js
@@ -2,7 +2,7 @@
 
 // FONT REPLACEMENT
 // Replace all tags with cufon equivalant
-Cufon.replace('h1,h2,h3,h4,h5,h6,.standardForm label');
+Cufon.replace('h1,h2,h3,.standardForm label');
 //
 
 // JQUERY
