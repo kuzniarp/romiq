fd9f7c2b2f08259856fce8c8802720c3fc23cbf1
kuzniarp <kuzniarp@gmail.com>
2010-12-18 21:29:20 +0100

    Added gallery slider on hp.

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,7 +4,13 @@ class Work < ActiveRecord::Base
   has_many :category_items, :as => :item
   has_many :categories, :through => :category_items
 
+  named_scope :active, :conditions => {:status => true}
+
   def image
       pictures.first
+  end
+
+  def self.newest limit=1
+      active.find(:all, :limit => limit, :order => "created_at desc")
   end
 end
