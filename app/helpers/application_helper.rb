29e5b82ee3a81c5a34248fbf1355e7a386755e1a
kuzniarp <kuzniarp@gmail.com>
2011-02-25 09:02:33 +0100

    Refactored category list method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def items_category_path cat
+    case cat.class.to_s 
+    when 'ProductCategory' then products_category_path(cat)
+    when 'WorkCategory' then works_category_path(cat)
+    end
+  end
   
   def item_path item
     case item.class.to_s 
@@ -23,7 +30,7 @@ module ApplicationHelper
     list = ''
     roots = model.roots
     roots.each do |root|
-      list += content_tag(:ul, content_tag(:li, link_to_if(active != root, root.name, (admin ? edit_admin_category_path(root) : (model == ProductCategory ? products_category_path(root) : works_category_path(root))), :class => ('active' if active == root)) + (link_to(" "+image_tag("icons/plus.png"), new_admin_category_path(:parent_id => root.id, :type => model), :title => "Dodaj podkategorie") if admin).to_s + content_tag(:p, root.items.map{|item| link_to_if(active_item != item, item.name, item_path(item))}.join(', '), :class => "small")+ subcategory_list(root, admin, active, active_item)), :class => 'root')
+      list += content_tag(:ul, content_tag(:li, link_to_if(active != root, root.name, (admin ? edit_admin_category_path(root) : items_category_path(root)), :class => ('active' if active == root)) + (link_to(" "+image_tag("icons/plus.png"), new_admin_category_path(:parent_id => root.id, :type => model), :title => "Dodaj podkategorie") if admin).to_s + content_tag(:p, root.items.map{|item| link_to_if(active_item != item, item.name, item_path(item))}.join(', '), :class => "small")+ subcategory_list(root, admin, active, active_item)), :class => 'root')
     end
     list += link_to("Dodaj kategorie glowna", new_admin_category_path(:type => model)) if admin
     list
@@ -32,7 +39,7 @@ module ApplicationHelper
   def subcategory_list root, admin, active, active_item
     list = ''
     root.children.each do |cat|
-      list += content_tag(:li, link_to_if(active != cat, cat.name, (admin ? edit_admin_category_path(cat) : (cat.class == ProductCategory ? products_category_path(cat) : works_category_path(cat))), :class => ('active' if active == cat)) + (link_to(" "+image_tag("icons/plus.png"), new_admin_category_path(:parent_id => cat.id, :type => cat.class), :title => "Dodaj podkategorie") if admin).to_s + content_tag(:p, cat.items.map{|item| link_to_if(active_item != item, item.name, product_path(item))}.join(', '), :class => "small") + subcategory_list(cat, admin, active, active_item))
+      list += content_tag(:li, link_to_if(active != cat, cat.name, (admin ? edit_admin_category_path(cat) : items_category_path(cat)), :class => ('active' if active == cat)) + (link_to(" "+image_tag("icons/plus.png"), new_admin_category_path(:parent_id => cat.id, :type => cat.class), :title => "Dodaj podkategorie") if admin).to_s + content_tag(:p, cat.items.map{|item| link_to_if(active_item != item, item.name, product_path(item))}.join(', '), :class => "small") + subcategory_list(cat, admin, active, active_item))
     end  
     #	  list += content_tag(:li, link_to("Nowa", new_admin_category_path)) if admin
     list.present? ? content_tag(:ul, list, :style => 'margin:0;') : ''
