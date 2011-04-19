0b7ea4d9d38050436e7f67cec827152e8e793a90
kuzniarp <kuzniarp@gmail.com>
2011-04-19 09:51:42 +0200

    Added recent post to the posts index page.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
-      
-      def index
-      	  @posts = Post.recent
-      end
-
-      def show
-      	  @post = Post.find_by_id(params[:id])	  	  
+  
+  def index
+    @posts = Post.recent
+	  @recent = Post.recent(params[:id])
+  end
+  
+  def show
+    @post = Post.find_by_id(params[:id])	  	  
 	  @recent = Post.recent(params[:id])
 	  render :text => "Nie znaleziono wpisu." unless @post
-      end   
+  end   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,27 @@
 class Post < ActiveRecord::Base
 
-      named_scope :active, :conditions => {:status => true}
-#      named_scope :recent, :lambda {|exclude_id| :conditions => ["id != ?", exclude_id], :order => "created_at desc"}
+  named_scope :active, :conditions => {:status => true}
+  named_scope :for_month, lambda {|date| {:conditions => ["created_at between :from and :to", {:from => date, :to => date}]}}
 
-      def self.newest
-      	  active.find(:first, :order => "created_at desc")
-      end   
+  MONTHS = ["Styczen", "Luty", "Marzec", "Kwiecien", "Maj", "Czerwiec", "Lipiec", "Sierpien", "Wrzesien", "Pazdziernik", "Listopad", "Grudzien"]
+  
+  def self.newest
+    active.find(:first, :order => "created_at desc")
+  end   
+  
+  def self.recent exclude_id=nil, limit=10
+    active.find(:all, :order => "created_at desc", :conditions => ["id != ?", exclude_id.to_i], :limit => limit)
+  end
 
-      def self.recent exclude_id=nil, limit=10
-      	  active.find(:all, :order => "created_at desc", :conditions => ["id != ?", exclude_id.to_i], :limit => limit)
+  def self.months
+    months = {}
+    all(:order => "created_at").group_by{|p| p.created_at.year}.each do |year,posts|
+      posts.each do |post|
+        months[year] = [] unless months[year]
+        month = MONTHS[post.created_at.month-1]
+        months[year] << month unless months[year].include?(month)
       end
+    end
+    months
+  end
 end
