2ce377963d1b53b16b69786295dd2c9dcc2ec1f3
kuzniarp <kuzniarp@gmail.com>
2011-06-23 22:23:46 +0200

    Updated contact eamil recipients.

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
 class Mailer < ActionMailer::Base  
+  RECIPIENTS = {"Administracja" => "biuro@romiq.pl", "Fronty Meblowe" => "krzysztof@romiq.pl", "Obsługa Klienta Indywidualnego" => "mirek@romiq.pl", "Obsługa Firm" => "mirek@romiq.pl"}
+
   def mail subject, email, message, branch, sent_at = Time.now
     @subject    = subject
     @body       = {:message => message}
-    @recipients = "kuzniarp@gmail.com"
+    @recipients = RAILS_ENV == 'production' ? RECIPIENTS[branch] : 'kuzniarp@gmail.com'
     @from       = email
     @sent_on    = sent_at
     @content_type = 'text/html'    
